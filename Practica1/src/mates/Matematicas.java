f6e8330948b5ec0922b58de5fc4bcbcf14e017ea
rubencerroo <ruben.cerrodiaz@usp.ceu.es>
2023-02-07 11:08:30 +0100

    Practica1v2

--- Practica1/src/mates/Matematicas.java
+++ Practica1/src/mates/Matematicas.java
@@ -1,24 +1,21 @@
 package mates;
-import java.util.*;
+import java.util.Random;
+
 public class Matematicas {
-	  /**
-	   * Genera una aproximación al número pi mediante el método de Montecarlo.
-	   * El parámetro ‘pasos‘ indica el número de puntos generado.
-	   */
-	  public static double generarNumeroPi(long pasos) {
-	    int aciertos = 0;
-	    double areaCuadrado = 4;
-	    Random rand = new Random();
-	    
-	    for (int i = 0; i < pasos; i++) {
-	      double x = rand.nextDouble() * 2 - 1;
-	      double y = rand.nextDouble() * 2 - 1;
-	      if (x * x + y * y <= 1) {
-	        aciertos++;
-	      }
-	    }
-	    
-	    return areaCuadrado * (aciertos / (double) pasos);
-	  }
-	}
+  public static double generarNumeroPi(long pasos) {
+    int aciertos = 0;
+    double areaCuadrado = 4.0;
+    Random random = new Random();
+
+    for (int i = 0; i < pasos; i++) {
+      double x = random.nextDouble() * 2.0 - 1.0;
+      double y = random.nextDouble() * 2.0 - 1.0;
+      if (Math.sqrt(x * x + y * y) <= 1.0) {
+        aciertos++;
+      }
+    }
+
+    return areaCuadrado * ((double) aciertos / (double) pasos);
+  }
+}
 
